e467cd974cd7f36a31a3557752babfa1c36e5bb2
aetreon <aetreon.makeo@gmail.com>
2019-02-01 16:35:17 +0000

    Fix error on CircleCI #6

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -15,17 +15,6 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 git reset --hard
 cd ..
 
-# Delete Old publication
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-# Checking out gh-pages branch into public
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
-
 # Removing existing files
 echo "Removing existing files"
 rm -rf public/*
