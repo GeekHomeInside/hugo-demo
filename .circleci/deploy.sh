02742df2b9a710d2cac0ba9d8906133c4cb1b958
aetreon <aetreon.makeo@gmail.com>
2019-02-01 16:28:57 +0000

    Fix error on CircleCI #4

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+TARGET_BRANCH="gh-pages"
+
+# Set Directory
 DIR=$(dirname "$0")
 
 cd $DIR/..
@@ -10,22 +17,27 @@ then
     exit 1;
 fi
 
+# Delete Old publication
 echo "Deleting old publication"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
+# Checking out gh-pages branch into public
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public upstream/gh-pages
 
+# Removing existing files
 echo "Removing existing files"
 rm -rf public/*
 
+# Generating site
 echo "Generating site"
 hugo -t meghna-hugo
 
+# Updating gh-pages branch
 echo "Updating gh-pages branch"
 cd public && git config --global user.name "aetreon" && git config --global user.email "aetreon.makeo@gmail.com" && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-git push upstream gh-pages
+git push $SSH_REPO $TARGET_BRANCH
